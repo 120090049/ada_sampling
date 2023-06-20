a50ac258ba2e3b90158468f3cced61e484a6b3b7
120090049 <120090049@link.cuhk.edu.cn>
2023-06-20 09:13:06 +0800

    test

--- scripts/uav_control/main_controller.py
+++ scripts/uav_control/main_controller.py
@@ -1,12 +1,13 @@
 import subprocess
 
-def open_new_terminal():
+def open_new_terminal(num):
     print("正在打开新的终端...")
-    subprocess.Popen(["gnome-terminal", "--", "bash", "-c", "python3 hello_world.py; sleep " + str(num)])
+    script_cmd = "python3 hello_world.py " + str(num) + ";"
+    subprocess.Popen(["gnome-terminal", "--", "bash", "-c", script_cmd,  "; sleep infinity" ])
 
 if __name__ == "__main__":
     while True:
         num = input("请输入一个数字：")
         if (int(num) == 1):
-            open_new_terminal()
+            open_new_terminal(num)
     
