487d68af9cd79c2f023debd085350f48f64a1f1d
120090049 <120090049@link.cuhk.edu.cn>
2023-06-29 11:42:32 +0800

    ship

--- scripts/ship_controller.py
+++ scripts/ship_controller.py
@@ -1,41 +1,46 @@
 #!/usr/bin/python3
-
 import rospy
-from gazebo_msgs.srv import SetModelState
+from gazebo_msgs.srv import SetModelState, GetModelState
 from gazebo_msgs.msg import ModelState
-from geometry_msgs.msg import Pose, Twist
+from geometry_msgs.msg import Pose
 
 def move_vessel():
     rospy.init_node('vessel_controller')
-
+    rate = rospy.Rate(20)
     # 创建服务客户端
     rospy.wait_for_service('/gazebo/set_model_state')
     set_model_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+    get_model_state = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
 
     model_state = ModelState()
     model_state.model_name = 'vessel_g_1'
 
-    # 设置新的模型位置
-    # model_state.pose.position.x = 1.0  # 设置x坐标为1.0
-    # model_state.pose.position.y = 2.0  # 设置y坐标为2.0
-    # model_state.pose.position.z = 0.0  # 设置z坐标为0.0
+    # 获取当前模型位置
+    current_state = get_model_state(model_state.model_name, "")
 
-    # 设置模型速度（如果需要）
-    model_state.twist.linear.x = 0.1  # 设置x方向的线速度为0.1 m/s
-    model_state.twist.linear.y = 0.0
-    model_state.twist.linear.z = 0.0
+    # 设置初始位置和步长
+    initial_x = current_state.pose.position.x
+    target_x = initial_x + 0.1  # 目标位置为当前位置+0.1m
 
-    # 创建Pose和Twist的空消息，用于姿态和速度的默认值
-    pose = Pose()
-    twist = Twist()
+    # 循环控制模型移动
+    while True:
+        # 设置新的模型位置
+        model_state.pose.position.x = current_state.pose.position.x + 0.01  # 每次前进0.01m
 
-    # 设置模型的姿态和速度
-    # model_state.pose = pose
-    model_state.twist = twist
+        # 创建Pose的空消息，用于姿态的默认值
+        pose = Pose()
 
-    # 发送服务请求，将模型状态更新为新位置和速度
-    set_model_state(model_state)
+        # 设置模型的姿态
+        model_state.pose = pose
 
+        # 发送服务请求，将模型状态更新为新位置
+        set_model_state(model_state)
+
+        # 获取更新后的模型位置
+        current_state = get_model_state(model_state.model_name, "")
+
+        rate.sleep()
+        
 if __name__ == '__main__':
     try:
         move_vessel()
