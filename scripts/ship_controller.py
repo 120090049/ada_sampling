b85793ec066f09e6a77d904dd43f3b22034bfe67
120090049 <120090049@link.cuhk.edu.cn>
2023-06-29 21:28:13 -0400

    para

--- scripts/ship_controller.py
+++ scripts/ship_controller.py
@@ -13,32 +13,21 @@ def move_vessel():
     get_model_state = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
 
     model_state = ModelState()
-    model_state.model_name = 'vessel_g_1'
+    model_state.model_name = 'vessel_d_1'
 
-    # 获取当前模型位置
-    current_state = get_model_state(model_state.model_name, "")
-
-    # 设置初始位置和步长
-    initial_x = current_state.pose.position.x
-    target_x = initial_x + 0.1  # 目标位置为当前位置+0.1m
-
+ 
     # 循环控制模型移动
     while True:
         # 设置新的模型位置
-        model_state.pose.position.x = current_state.pose.position.x + 0.01  # 每次前进0.01m
 
-        # 创建Pose的空消息，用于姿态的默认值
-        pose = Pose()
-
-        # 设置模型的姿态
-        model_state.pose = pose
+        current_state = get_model_state(model_state.model_name, "")
+        model_state.pose.position.x = current_state.pose.position.x + 0.02  
 
         # 发送服务请求，将模型状态更新为新位置
         set_model_state(model_state)
 
         # 获取更新后的模型位置
-        current_state = get_model_state(model_state.model_name, "")
-
+        print(int(model_state.pose.position.x), model_state.pose.position.y, model_state.pose.position.z, end="\r")
         rate.sleep()
         
 if __name__ == '__main__':
